bb3b5c888271870885c3ebab93088dcde1ce9937
Omid <omid@Omids-MacBook-Pro.local>
2019-12-03 20:50:53 -0800

    clean up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
 import React, { useState, useEffect } from "react";
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "./App.css";
+import { db } from "./firebase";
 
-// Firebase configuration for snatch-b94e3
-const firebaseConfig = {
-  apiKey: "AIzaSyB2rRbMgGNT-Za0SfEMekz4M01nXK9_yFA",
-  authDomain: "snatch-b94e3.firebaseapp.com",
-  databaseURL: "https://snatch-b94e3.firebaseio.com",
-  projectId: "snatch-b94e3",
-  storageBucket: "snatch-b94e3.appspot.com",
-  messagingSenderId: "104535857240",
-  appId: "1:104535857240:web:d3f416720f9dc4125ad22d"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore();
-
-// remove hardcoded userId after authentication is set up
+// TODO: remove hardcoded userId after authentication is set up
 const userId = "0diqOSAmwzTzccIJFzwJKZihWxc2";
 
 function App() {
