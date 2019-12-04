15de17094165463d5e8f47f1fcc5e4d36453d19e
Omid <omid@Omids-MacBook-Pro.local>
2019-12-03 18:15:25 -0800

    setup firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import firebase from "firebase/app";
+import "./App.css";
+
+// Firebase configuration for snatch-b94e3
+const firebaseConfig = {
+  apiKey: "AIzaSyB2rRbMgGNT-Za0SfEMekz4M01nXK9_yFA",
+  authDomain: "snatch-b94e3.firebaseapp.com",
+  databaseURL: "https://snatch-b94e3.firebaseio.com",
+  projectId: "snatch-b94e3",
+  storageBucket: "snatch-b94e3.appspot.com",
+  messagingSenderId: "104535857240",
+  appId: "1:104535857240:web:d3f416720f9dc4125ad22d"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
