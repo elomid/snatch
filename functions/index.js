33a555307b124ca2b6c817cb9343cc0b0eefb513
Omid <omid@Omids-MacBook-Pro.local>
2019-12-07 18:55:10 -0800

    add saveUrl endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,72 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const db = admin.firestore();
+
+const got = require("got");
+const cors = require("cors")({ origin: true });
+
+const metascraper = require("metascraper")([
+  require("metascraper-author")(),
+  require("metascraper-date")(),
+  require("metascraper-description")(),
+  require("metascraper-image")(),
+  require("metascraper-logo")(),
+  require("metascraper-clearbit")(),
+  require("metascraper-publisher")(),
+  require("metascraper-title")(),
+  require("metascraper-url")()
+]);
+
+const extractMetaDataFromUrl = async targetUrl => {
+  const { body: html, url } = await got(targetUrl);
+  const metadata = await metascraper({ html, url });
+  return metadata;
+};
+
+exports.savePage = functions.https.onRequest((request, response) => {
+  // Receives POST reqeuests with the url and userId
+  // Writes a new page to the default "inbox" list of the user
+  cors(request, response, () => {
+    if (request.method !== "POST") {
+      return response.status(400).json({
+        message: "Not allowed"
+      });
+    }
+
+    if (request.method === "POST") {
+      if (request.body.url) {
+        try {
+          const userId = request.body.userId;
+          (async () => {
+            const data = await extractMetaDataFromUrl(request.body.url);
+            db.collection("users")
+              .doc(userId)
+              .collection("pages")
+              .add({
+                archived: false,
+                archivedAt: null,
+                createdAt: new Date(),
+                deleted: false,
+                deletedAt: null,
+                description: data.description,
+                listId: "inbox",
+                publisher: data.publisher,
+                title: data.title,
+                url: data.url
+              })
+              .then(writeResult => {
+                return response.status(200).json({ message: writeResult });
+              })
+              .catch(err => err);
+          })();
+        } catch (error) {
+          return response.status(500).json({
+            message: error
+          });
+        }
+      }
+    }
+  });
+});
