1c8812ff8d4f465757e58d2a0866f023d1e6e99b
Omid <omid@Omids-MacBook-Pro.local>
2019-12-08 16:25:34 -0800

    add show/hide archived button

--- src/Pages.js
+++ src/Pages.js
@@ -11,13 +11,43 @@ function Pages({ userId, listId }) {
     filterKey: "listId",
     filterValue: listId
   });
+  const [showArchived, setShowArchived] = useState(false);
+
+  const unarchivedPages = pages.filter(page => !page.archived);
+  const archivedPages = pages.filter(page => page.archived);
+
+  const toggleShowArchived = () => {
+    setShowArchived(currentShowArchived => !currentShowArchived);
+  };
 
   return (
-    <ul className="pages">
-      {pages.map(page => (
-        <Page key={page.id} page={page} userId={userId} />
-      ))}
-    </ul>
+    <div>
+      <div>
+        <ul className="pages">
+          {unarchivedPages.map(page => (
+            <Page key={page.id} page={page} userId={userId} />
+          ))}
+        </ul>
+        {
+          <button
+            className="button button-secondary"
+            onClick={toggleShowArchived}
+          >
+            {showArchived ? "Hide" : "Show"} {archivedPages.length} archived{" "}
+            {archivedPages.length === 1 ? "item" : "items"}
+          </button>
+        }
+      </div>
+      {showArchived && (
+        <div>
+          <ul className="pages pages--archived">
+            {archivedPages.map(page => (
+              <Page key={page.id} page={page} userId={userId} />
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
   );
 }
 
